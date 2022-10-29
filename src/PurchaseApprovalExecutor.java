03a6069ac06117463c16ca3c124576e5e03a0336
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2022-10-29 16:21:40 +0300

    Add: Implement approval method for President and set purchase limit rules

--- src/PurchaseApprovalExecutor.java
+++ src/PurchaseApprovalExecutor.java
@@ -2,6 +2,7 @@ import common.Type;
 import handlers.Approver;
 import handlers.Director;
 import handlers.Manager;
+import handlers.President;
 
 /**
  * Execution class of the application.
@@ -15,6 +16,9 @@ public class PurchaseApprovalExecutor {
         //Be free to modify method below this line
         Approver director = new Director();
         ApprovalChainGenerator.generate(director);
+        Approver president = new President();
+        ApprovalChainGenerator.generate(president);
+
         director.approve(1,400,Type.CONSUMABLES);
         director.approve(2,500,Type.CONSUMABLES);
         director.approve(3,600,Type.CONSUMABLES);
@@ -23,6 +27,10 @@ public class PurchaseApprovalExecutor {
         manager.approve(5, 5000, Type.PC);
         manager.approve(6, 5000, Type.PC);
         manager.approve(7, 3000, Type.CLERICAL);
-
+        System.out.println(" ");
+        president.approve(8,6000,Type.GADGETS);
+        president.approve(9,1000,Type.GAMING);
+        president.approve(10,2000,Type.CLERICAL);
+        president.approve(11,4000,Type.GAMING);
     }
 }
--- src/handlers/President.java
+++ src/handlers/President.java
@@ -6,9 +6,19 @@ import common.Type;
  * //TODO - Implement approval implementation for President level
  */
 public class President extends Approver {
+    public President (){
+        purchaseLimitsPerRole.put(Type.CONSUMABLES,1000);
+        purchaseLimitsPerRole.put(Type.CLERICAL,2000);
+        purchaseLimitsPerRole.put(Type.GADGETS,3000);
+        purchaseLimitsPerRole.put(Type.GAMING,5000);
+        purchaseLimitsPerRole.put(Type.PC,8000);
+    }
     @Override
     public void approve(int id, double cost, Type type) {
-
-
+        if (CANAPPROVE(cost, type)) {
+            System.out.println("President approved purchase with id " + id + " that costs " + cost);
+            return;
+        }
+        System.out.println("Purchase with id " + id + " needs approval from higher position than President.");
     }
 }
