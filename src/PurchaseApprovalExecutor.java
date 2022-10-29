51a848acea2565ab03bb0e5f5827de9ee1bae90c
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2022-10-29 16:45:17 +0300

    Add: Implement approval method for Vice President and set purchase limit rules

--- src/PurchaseApprovalExecutor.java
+++ src/PurchaseApprovalExecutor.java
@@ -1,8 +1,5 @@
 import common.Type;
-import handlers.Approver;
-import handlers.Director;
-import handlers.Manager;
-import handlers.President;
+import handlers.*;
 
 /**
  * Execution class of the application.
@@ -18,19 +15,30 @@ public class PurchaseApprovalExecutor {
         ApprovalChainGenerator.generate(director);
         Approver president = new President();
         ApprovalChainGenerator.generate(president);
-
+        Approver vicePresident = new VicePresident();
+        ApprovalChainGenerator.generate(vicePresident);
+        System.out.println(" ");
+        System.out.println("Requests for Director:");
         director.approve(1,400,Type.CONSUMABLES);
         director.approve(2,500,Type.CONSUMABLES);
         director.approve(3,600,Type.CONSUMABLES);
         System.out.println(" ");
+        System.out.println("Requests for Manager:");
         manager.approve(4, 15000, Type.CONSUMABLES);
         manager.approve(5, 5000, Type.PC);
         manager.approve(6, 5000, Type.PC);
         manager.approve(7, 3000, Type.CLERICAL);
         System.out.println(" ");
+        System.out.println("Requests for President:");
         president.approve(8,6000,Type.GADGETS);
         president.approve(9,1000,Type.GAMING);
         president.approve(10,2000,Type.CLERICAL);
         president.approve(11,4000,Type.GAMING);
+        System.out.println(" ");
+        System.out.println("Requests for Vice President:");
+        vicePresident.approve(12,6500, Type.PC);
+        vicePresident.approve(13,4400, Type.GAMING);
+        vicePresident.approve(14,701, Type.CONSUMABLES);
+        vicePresident.approve(15,3900, Type.GADGETS);
     }
 }
--- src/handlers/VicePresident.java
+++ src/handlers/VicePresident.java
@@ -7,9 +7,19 @@ import common.Type;
  * //TODO - Implement approval implementation for VicePresident level
  */
 public class VicePresident extends Approver {
+    public VicePresident (){
+        purchaseLimitsPerRole.put(Type.CONSUMABLES,700);
+        purchaseLimitsPerRole.put(Type.CLERICAL,1500);
+        purchaseLimitsPerRole.put(Type.GADGETS,2000);
+        purchaseLimitsPerRole.put(Type.GAMING,4500);
+        purchaseLimitsPerRole.put(Type.PC,6500);
+    }
     @Override
     public void approve(int id, double cost, Type type) {
-
-
+        if (CANAPPROVE(cost, type)) {
+            System.out.println("Vice President approved purchase with id " + id + " that costs " + cost);
+            return;
+        }
+        System.out.println("Purchase with id " + id + " needs approval from higher position than Vice President.");
     }
 }
